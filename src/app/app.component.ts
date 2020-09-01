ffbeb122e715269092dd13422dfddddacf94d8ec
SyahirahJumain <syahirahjumain97@gmail.com>
2020-09-01 21:32:10 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+    
+/*Empty array to add a new division*/
+public lists = [];
+
+/*Push text into a division*/
+  public newAddedTask;
+
+
+/*this function will not create a new division when the input is empty*/
+  public addToLists()
+  {
+    if (this.newAddedTask == ''){
+
+    }
+    else{
+      this.lists.push(this.newAddedTask);
+      this.newAddedTask = '';
+    }
+
+  }
+  
+/*this function will delete the input/task selected*/
+public deleteLists(num, lists){
+  console.log("test", lists);
+  if(confirm(`Are you sure to delete ${lists}?`)){
+    this.lists.splice(num, 1);
+  }
+
+}
+ 
+
 }
